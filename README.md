dafd96f6d41966f8b72fbf6f9eb04cb439e1a19f
Brian Zelip <brian.zelip@lyrasis.org>
2023-10-16 06:46:38 -0400

    Add test and metadata

---
+++ README.md
@@ -0,0 +1,34 @@
+# Lapswim
+
+Menu bar app for displaying today's lap swim schedule at the Waverly Y in Baltimore.
+
+## Code provenance
+
+```sh
+# init cli choices:
+# - JS/!TS
+# - tests/ dir
+# - do not auto install 3 browsers
+npm init playwright@latest
+
+# Install only Firefox
+npx playwright install firefox
+
+# Run tests headless
+npx playwright test
+
+# Run tests in UI mode
+npx playwright test --ui
+```
+
+## Contributing
+
+Open a PR!
+
+## Author
+
+Brian Zelip, https://zelip.me
+
+## License
+
+GPLv3
--- tests/waverly_schedules.spec.js
+++ tests/waverly_schedules.spec.js
@@ -1,10 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
+  console.log(`Running ${testInfo.title}`);
   await page.goto(
     'https://ymaryland.org/locations/weinbergy/weinbergschedules'
   );
+});
 
-  // Expect a title "to contain" a substring.
+test(`has 'Swim' in the page title`, async ({ page }) => {
   await expect(page).toHaveTitle(/Swim/);
+});
+
+test(`has today's schedule`, async ({ page }) => {
+  // Schedule header reads "Monday, October 15"
+  const today = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'America/New_York'
+  });
+
+  await expect(page.getByText(today)).toBeVisible();
 });
